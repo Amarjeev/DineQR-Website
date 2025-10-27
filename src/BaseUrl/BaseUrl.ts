151d8a396d68d7ad79bd30795698e7ddaa6f55f2
Amarjeev Ms <amarjeevm@gmail.com>
2025-10-27 16:30:59 +0500

    Baseurl updated

--- src/BaseUrl/BaseUrl.ts
+++ src/BaseUrl/BaseUrl.ts
@@ -1,4 +1,15 @@
-export const BaseUrl: string =
+// export const BaseUrl: string =
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:5000/api/v1/" // for development
+//     : "https://dineqr-backend-3.onrender.com/api/v1/"; // for production
+
+import { io } from "socket.io-client";
+
+export const BaseUrl = io(
   window.location.hostname === "localhost"
-    ? "http://localhost:5000/api/v1/" // for development
-    : "https://dineqr-backend-3.onrender.com/api/v1/"; // for production
+    ? "http://localhost:5000"
+    : "https://dineqr-backend-3.onrender.com/api/v1/",
+  {
+    transports: ["websocket"],
+  }
+);
