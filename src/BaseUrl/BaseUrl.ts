9316750e6f9276e9a7ce74099d738670b59d178f
Amarjeev Ms <amarjeevm@gmail.com>
2025-10-27 16:37:38 +0500

    base url and socket io backend link added

--- src/BaseUrl/BaseUrl.ts
+++ src/BaseUrl/BaseUrl.ts
@@ -1,15 +1,15 @@
 // export const BaseUrl: string =
-//   window.location.hostname === "localhost"
-//     ? "http://localhost:5000/api/v1/" // for development
-//     : "https://dineqr-backend-3.onrender.com/api/v1/"; // for production
-
-import { io } from "socket.io-client";
-
-export const BaseUrl = io(
   window.location.hostname === "localhost"
-    ? "http://localhost:5000"
-    : "https://dineqr-backend-3.onrender.com/api/v1/",
-  {
-    transports: ["websocket"],
-  }
-);
+    ? "http://localhost:5000/api/v1/" // for development
+    : "https://dineqr-backend-3.onrender.com/api/v1/"; // for production
+
+// import { io } from "socket.io-client";
+
+// export const BaseUrl = io(
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:5000"
+//     : "https://dineqr-backend-3.onrender.com/api/v1/",
+//   {
+//     transports: ["websocket"],
+//   }
+// );
