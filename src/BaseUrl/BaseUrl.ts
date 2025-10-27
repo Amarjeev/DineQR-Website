5e0294dbf6acac005135a1672952b871d62edd34
Amarjeev Ms <amarjeevm@gmail.com>
2025-10-27 15:51:46 +0500

    added backend link

--- src/BaseUrl/BaseUrl.ts
+++ src/BaseUrl/BaseUrl.ts
@@ -1,4 +1,4 @@
 export const BaseUrl: string =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api/v1/" // for development
-    : "https://dineqr-backend.onrender.com/api/v1/"; // for production
+    : "https://dineqr-backend-3.onrender.com/api/v1/"; // for production
