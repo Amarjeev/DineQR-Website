5a98b170272e9801224252448362dfd4fa0f7be1
Amarjeev Ms <amarjeevm@gmail.com>
2025-10-30 14:56:11 +0500

    Update: Base URL switched to https://api.dineqr.cfd for production

--- src/BaseUrl/BaseUrl.ts
+++ src/BaseUrl/BaseUrl.ts
@@ -1,6 +1,6 @@
 // 🌍 API Base URL (Render Backend)
 export const BaseUrl: string = 'https://dineqr-backend-3.onrender.com/api/v1/'
-// export const BaseUrl: string = "http://localhost:4000/api/v1/";
+
 
 // 🔌 Socket URL
 export const SocketUrl: string = 'https://dineqr-backend-3.onrender.com'
